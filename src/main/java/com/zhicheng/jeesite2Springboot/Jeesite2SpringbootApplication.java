5da090c398c32f0eac9ccdb00ef66e3473c9b3bf
fengsong97 <973017388@qq.com>
2018-06-05 19:15:33 +0800

    jsp test

--- src/main/java/com/zhicheng/jeesite2Springboot/Jeesite2SpringbootApplication.java
+++ src/main/java/com/zhicheng/jeesite2Springboot/Jeesite2SpringbootApplication.java
@@ -2,9 +2,18 @@ package com.zhicheng.jeesite2Springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Jeesite2SpringbootApplication {
+public class Jeesite2SpringbootApplication  extends SpringBootServletInitializer {
+
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Jeesite2SpringbootApplication.class);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Jeesite2SpringbootApplication.class, args);
