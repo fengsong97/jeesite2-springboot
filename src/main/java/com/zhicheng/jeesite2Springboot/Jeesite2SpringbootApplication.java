ceaf7b1dce63a038624b28a53828a34625e2f23b
fengsong97 <973017388@qq.com>
2018-06-05 17:56:09 +0800

    初始化

---
+++ src/main/java/com/zhicheng/jeesite2Springboot/Jeesite2SpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.zhicheng.jeesite2Springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jeesite2SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jeesite2SpringbootApplication.class, args);
+	}
+}
